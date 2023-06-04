fcb912ec6df8fd9aee078775de143edc92069069
Sultepronit <sultepronit@gmail.com>
2023-06-04 13:55:39 +0300

    copied/refactored

---
+++ desktop/main.js
@@ -0,0 +1,243 @@
+'use strict';
+
+const wordsDb = [];
+let sessionLength = 0, maxToRepeat = 0, nextRepeated = 0;
+
+let repeatList = [];
+const problemList = [];
+const recognizeList = [];
+const sessionList = [];
+
+let numberToRecognize = 0, numberWithProblem = 0, numberToRepeat = 0;
+
+let progress = '';
+let direction = '';
+
+//let sessionLength = sessionList.length;
+
+function contactServer(mes) {
+	let src = "http://localhost:5050/" + mes;
+	$.getScript(src, function(){
+		//console.log("Success!");
+	}).fail(function () {
+		alert("No connection!");
+		//console.log("No connection!");
+	});
+}
+
+function showStats() {
+	$('.stats').empty();
+	let re = '<b>';
+	let cn = sessionLength - sessionList.length;
+	let pc = Math.round(cn / sessionLength * 100);
+	re += cn + '/' + sessionLength + ': ' + pc + '%</b>';
+	
+	re += ' | <span class="green">wl-' + numberToLearn + ': ';
+	re += wordLearnPlus + '-' + wordLearnMinus;
+	re += '<b> ' + wordLearned + '</b></span>';
+	re += ' | <span class="green">wc-' + numberToConfirm + ': '; 
+	re += wordConfirmPlus + '-' + wordConfirmMinus;
+	re += '<b> ' + wordConfirmed + '-' + wordNotConfirmed + '</b></span>';
+	re += ' | wr-' + numberToRepeat + '/' + numberWithProblem + ': ';
+	re += wordRepeatPlus + '<sup>' + wordRepeatPlusAuto + '</sup>';
+	re += '-' + wordRepeatMinus + '<b> ' + wordRepeated;
+	re += '<sup>' + wordRepeatedAuto + '</sup>-' + wordReturned + '</b>';
+	re+= ' | <span class="blue">[' + numberToRecognize + '] <b>';
+	re += wordSuperPlus + '-' + wordSuperMinus + '</b></span>';
+	
+	re += ' || <span class="green">kl-' + numberToLearnKanji + ': ';
+	re += kanjiLearnPlus + '-' + kanjiLearnMinus;
+	re += '<b> ' + kanjiLearned + '</b></span>';
+	re += ' | kr: ' + kanjiRepeatPlus + '<sup>' + kanjiRepeatPlusAuto + '</sup>';
+	re += '-' + kanjiRepeatMinus + '<b> ' + kanjiRepeated;
+	re += '<sup>' + kanjiRepeatedAuto + '</sup>-' + kanjiReturned + '</b>';
+	re += ' | <span class="blue"><b>' + kanjiSuperPlus + '-' + kanjiSuperMinus + '</b></span>';
+	
+	$('.stats').append(re);
+}
+
+function pressedG() {
+	switch(progress) {
+		case 'FIRST_EVALUATION':
+			if(direction !== 'FORWARD') break;
+			$(".word").css("border-bottom", "6px solid green");
+			wordFirstMark = "GOOD";
+			break;
+		case 'WORD_EVALUATION':
+			$(".word-panel").css("border", "6px solid green");
+			wordMark = "GOOD";
+			break;
+		case 'KANJI_EVALUATION':
+			$(".kanji-panel").css("border", "6px solid green");
+			kanjiMark = "GOOD";
+			break;
+		case 'RECOGNITION_EVALUATION':
+			$(".word-panel").css("border", "6px solid green");
+			wordMark = "GOOD";
+			break;
+		default: 
+			console.log(progress);
+	}
+}
+
+function pressedB() {
+	switch(progress) {
+		case 'FIRST_EVALUATION':
+			if(direction !== 'FORWARD') break;
+			$(".word").css("border-bottom", "6px solid red");
+			wordFirstMark = "BAD";
+			break;
+		case 'WORD_EVALUATION':
+			$(".word-panel").css("border", "6px solid red");
+			wordMark = "BAD";
+			break;
+		case 'KANJI_EVALUATION':
+			$(".kanji-panel").css("border", "6px solid red");
+			kanjiMark = "BAD";
+			break;
+		case 'RECOGNITION_EVALUATION':
+			$(".word-panel").css("border", "6px solid red");
+			wordMark = "BAD";
+			break;
+		default: 
+			console.log(progress);
+	}
+}
+
+function pressedN() {
+	switch(progress) {
+		case "FIRST_EVALUATION":
+			if(direction !== 'FORWARD') break;
+			$(".word").css("border-bottom", "6px solid yellow");
+			wordFirstMark = "NEUTRAL";
+			break;
+		case "WORD_EVALUATION":
+			$(".word-panel").css("border", "6px solid yellow");
+			wordMark = "NEUTRAL";
+			break;
+		default:
+			console.log(progress);
+	}
+}
+
+function pressedV() {
+	switch(progress) {
+		case "WORD_EVALUATION":
+			if(direction !== "BACKWARD") return;
+			$(".word-panel").css("border", "6px solid blue");
+			wordMark = "BEST";
+			break;
+		case 'KANJI_EVALUATION':
+			$(".kanji-panel").css("border", "6px solid blue");
+			kanjiMark = "BEST";
+			break;
+		default:
+			console.log(progress);
+	}
+}
+
+function pressedEnter() {
+	switch(progress) {
+		case "WORD_QUESTION":
+			showFirstAnswer();
+			break;
+		case "FIRST_EVALUATION":
+			saveFirstResult();
+			break;
+		case "WORD_EVALUATION":
+			saveProgressWord();
+			break;
+		case 'KANJI_QUESTION':
+			showAnswerKanji();
+			break;
+		case 'KANJI_EVALUATION':
+			saveProgressKanji();
+			break;
+		case 'READY_TO_GO':
+			nextCard();
+			break;
+		case 'RECOGNITION':
+			showAnswerToRecognize();
+			break;
+		case 'RECOGNITION_EVALUATION':
+			saveRecognitionProgress();
+			break;
+		default: 
+			console.log(progress);
+	}
+}
+
+$(document).on("keypress", function (event) {
+
+	//console.log(event.keyCode);
+	$(".control").text(event.key);
+
+	switch(event.keyCode) {
+		case 97: 
+			playAudio(0);
+			break;
+		case 13: 
+			pressedEnter();
+			break;
+		case 103:
+			pressedG();
+			break;
+		case 98:
+			pressedB();
+			break;
+		case 110:
+			pressedN();
+			break;
+		case 118: 
+			pressedV();
+			break;
+		case 104:
+			pressedH();
+			break;
+		case 112:
+			pressedP();
+			break;
+	}
+
+});
+
+function clearList() {
+	$('.single').empty();
+	$('.right-half').empty();
+	$('.left-half').empty();
+}
+
+function nextCard() {
+	//showStats();
+
+	clearList();
+	
+	if(sessionList.length < 1) {
+		$('.word').empty().append('Happy End!');
+		return;
+	}
+	let cardStatus = deleteRandomFromArray(sessionList);
+	console.log(cardStatus);
+	//console.log(sessionList);
+	
+	if(cardStatus[0] == 'K') {
+		kanjiStatus = cardStatus;
+		nextKanji();
+		return;
+	}
+	if(cardStatus === 'RECOGNIZE') {
+		nextRecognition();
+		return;
+	}
+	wordStatus = cardStatus;
+	nextWord();
+	
+	//nextKanji();
+	//nextRecognition();
+}
+
+const main = function () {
+	console.log('Document is ready!');
+};
+	
+$(document).ready(main);
---
+++ main/word-cycle.js
@@ -0,0 +1,340 @@
+"use strict";
+
+let wordStatus;
+let currentWordId = 0;
+let currentWord = {}, unchangedCard = {};
+let writings;
+
+let wordFirstMark = '';
+let wordMark = '';
+
+let learnPlus = 0,  wordLearnMinus = 0, wordLearned = 0;
+let wordConfirmPlus = 0, wordConfirmMinus = 0, wordConfirmed = 0;
+let wordRepeatPlus = 0, wordRepeatMinus = 0, wordRepeated = 0;
+let wordRepeatPlusAuto = 0, wordRepeatedAuto = 0;
+let wordReturned = 0, wordNotConfirmed = 0;
+let wordSuperPlus = 0, wordSuperMinus = 0;
+
+function nextWord() {
+	/*$('.kanji-panel').hide();
+	$('.word-panel').show();*/
+	
+	if(randomFromRange(0,1)) {
+		$('.word').css('font-family', '"Noto Serif JP", serif');
+	} else {
+		$('.word').css('font-family', '"Noto Sans JP", sans-serif');
+	}
+	
+	switch(wordStatus) {
+		case 'LEARN':
+			currentWordId = deleteRandomFromArray(learnList);
+			//console.log(learnList);
+			break;
+		case 'CONFIRM':
+			currentWordId = deleteRandomFromArray(confirmList);
+			break;
+		case 'PROBLEM':
+			currentWordId = deleteRandomFromArray(problemList);
+			break;
+		case 'REPEAT':
+			currentWordId = deleteRandomFromArray(repeatList);
+			break;
+		default:
+			console.log("!!!" + wordStatus);
+	}
+	//currentWordId = randomFromRange(0, wordsDb.length -1);
+	
+	currentWord = wordsDb[currentWordId];
+	unchangedCard = Object.assign({}, currentWord);
+	//console.log(currentWordId);
+	console.log(currentWord);
+	
+	writings = processWritings(currentWord);
+	//console.log(writings);
+	
+	wordFirstMark = 'UNEVALUATED';
+	wordMark = 'UNEVALUATED';
+	
+	/*direction = 'FORWARD';
+	direction = 'BACKWARD';
+	let of2 = randomFromRange(0,1);*/
+	//direction = of2 ? 'FORWARD' : 'BACKWARD';
+	//direction = (currentWord.f > currentWord.b) ? 'BACKWARD' : 'FORWARD';
+	
+	if(currentWord.f > currentWord.b) {
+		direction = 'BACKWARD';
+	} else {
+		direction = 'FORWARD';
+		if(currentWord.ff > 2.1) {
+			console.log('Word To PASS! [F]');
+			
+			currentWord.f++;
+			
+			currentWord.ff = 2.1;
+			wordRepeatPlusAuto++;
+			
+			showEverything();
+			return;
+		}
+	}
+	
+	//if(currentWord.b > 1) direction = 'FORWARD';
+	if(currentWord.ff <= 0 && currentWord.s < 2) direction = 'FORWARD';
+	
+	showQuestionWord();
+}
+
+function showEverything() {
+	sessionList.push('REPEAT');
+	
+	direction = 'BACKWARD';
+	showQuestionWord();
+	showFirstAnswer();
+	showAnswerWord();
+	$(".word-panel").css("border", "6px solid black");
+	
+	progress = 'READY_TO_GO';
+	$('.evaluation').hide();
+	$('.next').show();
+	
+	sendWordChanges();
+}
+
+function showQuestionWord() {
+	progress = 'WORD_QUESTION';
+	$('.evaluation').hide();
+	$('.next').show();
+	
+	$('.card-info').empty();
+	$('.kanji-list').empty();
+	$('.word').empty();
+	$('.gifs').empty();
+	$('.transcription').empty();
+	$('.translation').empty();
+	$('.example').empty();
+	$(".word").css("border-bottom", "6px solid white");
+	$(".word-panel").css("border", "6px solid white");
+	
+	let info = currentWordId + ' [' + currentWord.s + "]:\t";
+	info += currentWord.f + " " + currentWord.b + " | " + currentWord.ff;
+	$('.card-info').append(info);
+	
+	if(direction === "FORWARD") {
+		let rw = randomFromRange(0, writings.mainWritings.length - 1);
+		$('.word').append(writings.mainWritings[rw]);
+	} else { // BACKWARD
+		$('.translation').append(currentWord.tsl);
+	}
+}
+
+function showFirstAnswer() {
+	progress = "FIRST_EVALUATION";
+	if(direction === 'FORWARD') {
+		$('.evaluation').show();
+		$('.next').hide();
+	}
+	//$('.transcription').text(currentWord.tsc);
+	$('.transcription').text(writings.kana);
+	if(!muted) playAudio(0);
+}
+
+let kanjiFromWord = [];
+function showKanjiList() {
+	kanjiFromWord = [];
+	let n = 0;
+	for(let kanji of writings.allKanji) {
+		//console.log(kanji);	
+		let t = '';
+		let learning = false;
+		for(let card of kanjiDb) {
+			if(card.k == kanji) {
+				learning = true;
+				t += ' ' + card.n + ' <i>' + jooyoo[card.j][4] + '</i>';
+				break;
+			}
+		}
+		
+		if(!learning) {
+			for(let card of jooyoo) {
+				if(card[0] == kanji) {
+					t += ' <i>' + card[4] + '</i>';
+					break;
+				}
+			}
+		}
+		
+		let $kanji = $('<span class="k" onmousedown="showKanji('+n+')">');
+		$kanji.append(kanji);
+
+		let $about = $('<span id="k'+n+'">').append(t);
+		$about.hide();
+		$kanji.append($about);
+		$('.kanji-list').append($kanji);
+		kanjiFromWord.push(kanji);
+		n++;
+	}
+}
+
+function showAnswerWord() {
+	progress = "WORD_EVALUATION";
+	if(direction === 'BACKWARD') {
+		$('.evaluation').show();
+		$('.next').hide();
+	}
+	//insertGifs(writings.allKanji);
+	showKanjiList();
+	$('.example').append(example[currentWord.e]);
+	$('.word').empty().append(writings.allWritings);
+	
+	if(direction === "FORWARD") {
+		$('.translation').append(currentWord.tsl);
+	} else { // BACKWARD
+		//$('.word').append(writings.allWritings);
+	}
+}
+
+function saveProgressWord() {
+	//if(wordMark === 'UNEVALUATED') return;
+	
+	// basic progress
+	if(currentWord.s == 0) {
+		if(wordMark === 'NEUTRAL') wordMark = 'BAD';
+		if(wordMark === 'BAD') {
+			sessionList.push('LEARN');
+			learnList.push(currentWordId);
+			console.log('I\'m back!');
+		}
+	}
+	let change = 0;
+	if(wordMark === 'GOOD' || wordMark === 'BEST') {
+		change++;
+		switch(wordStatus) {
+			case 'LEARN':
+				learnPlus++;
+				break;
+			case 'CONFIRM':
+				wordConfirmPlus++;
+				break;
+			default:
+				wordRepeatPlus++;
+		}
+	} else if(wordMark === 'BAD') {
+		change--;
+		switch(wordStatus) {
+			case 'LEARN':
+				wordLearnMinus++;
+				break;
+			case 'CONFIRM':
+				wordConfirmMinus++;
+				break;
+			default:
+				wordRepeatMinus++;
+		}
+	}
+
+	if(direction === 'FORWARD') {
+		currentWord.f += change;
+	} else { // BACKWARD
+		currentWord.b += change;
+	}
+	
+	// recognition/writing progress
+	if(direction === 'FORWARD') {
+		if(wordFirstMark === 'GOOD') {
+			wordFirstMark = wordMark;
+		} else if(wordFirstMark === 'NEUTRAL' && wordMark === 'BAD') {
+			wordFirstMark = 'BAD';
+		}
+
+		if(wordFirstMark === 'GOOD') {
+			currentWord.ff++;
+			wordSuperPlus++;
+		} else if(wordFirstMark === 'BAD') {
+			currentWord.ff--;
+			wordSuperMinus++;
+			if(currentWord.ff > 1) currentWord.ff = 1;
+		}
+		if(currentWord.ff < 0) {
+			currentWord.ff = 0;
+			wordSuperMinus--;
+		}
+		if(currentWord.ff > 1 && currentWord.s < 2) currentWord.ff = 1;
+	} /*else { // BACKWARD
+		if(wordMark === 'BEST') {
+			currentWord.bb++;
+			wordSuperPlus++;
+		} else if(wordMark !== 'NEUTRAL') {
+			currentWord.bb--;
+			wordSuperMinus++;
+			if(currentWord.bb > 1) currentWord.bb = 1;
+		}
+		if(currentWord.bb < 0) {
+			currentWord.bb = 0;
+			wordSuperMinus--;
+		}
+		if(currentWord.bb > 1 && currentWord.s < 2) currentWord.bb = 1;
+	}*/
+	
+	upgradeOrDegrade: {
+		//degrade
+		if(currentWord.f < -1 || currentWord.b < -1) {
+			if(wordStatus !== 'LEARN') { 
+				currentWord.s = 0;
+				currentWord.f = 0;
+				currentWord.b = 0;
+				if(wordStatus === 'CONFIRM') {
+					wordNotConfirmed++;
+				} else { //REPEAT
+					wordReturned++;
+				}
+				
+				maxToRepeat++;
+				sendRepeatStatus();
+			} else { //LEARN
+				if(currentWord.f < -1) { //forward
+					currentWord.f = -1;
+				} else { //backward
+					currentWord.f = 0;
+					currentWord.b = 0;
+				}
+			}
+			
+			break upgradeOrDegrade;
+		}
+		
+		//upgrade 
+		if(currentWord.f > 0 && currentWord.b > 0) {
+			if(wordStatus === 'REPEAT') {
+				currentWord.f = 0;
+				currentWord.b = 0;
+				currentWord.s = nextRepeated++;
+				wordRepeated++;
+				
+				maxToRepeat++;
+				sendRepeatStatus(true);
+				
+				break upgradeOrDegrade;
+			}
+			
+			if(currentWord.ff <= 0) break upgradeOrDegrade;
+			
+			currentWord.f = 0;
+			currentWord.b = 0;
+			
+			if(wordStatus === 'LEARN') {
+				currentWord.s = 1;
+				wordLearned++;
+				
+				maxToRepeat--;
+				sendRepeatStatus();
+			} else { // CONFIRM
+				currentWord.s = 2;
+				wordConfirmed++;
+			}
+		}
+	}
+	
+	sendWordChanges();
+	
+	nextCard();
+}
