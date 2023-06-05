f4075909c87ae3189b613b7492ad249b3d926189
Sultepronit <sultepronit@gmail.com>
2023-06-05 16:11:43 +0300

    corrected a little

--- googleSheetConnect-jap.js
+++ googleSheetConnect-jap.js
@@ -1,16 +1,15 @@
-var myApp = 'https://script.google.com/macros/s/AKfycbxdjsA65GpotgP1lTQ2hRcbt_JkxMnRWEo9K5wsrVKP-UfwxEiyoH_Lm4goQg9v-wxI/exec';
+let myApp = 'https://script.google.com/macros/s/AKfycbxdjsA65GpotgP1lTQ2hRcbt_JkxMnRWEo9K5wsrVKP-UfwxEiyoH_Lm4goQg9v-wxI/exec';
 
-//function getWordsDb (next) {
-	function getWordsDb () {
-    var action = "getTasks";
-    var url = myApp + "?action=" + action
+function getWordsDb () {
+    let action = "getTasks";
+    let url = myApp + "?action=" + action
 
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-			var wordsDb = JSON.parse(xhr.response);
+			let wordsDb = JSON.parse(xhr.response);
 			console.log(wordsDb.length);
 			refactorDb(wordsDb);
 			//console.log(wordsDb);
@@ -23,9 +22,9 @@ function toCell(num, col, newValue)
 {	
 	num++;
 	console.log(col + num + ': ' + newValue);
-	var action = "toCell";
-	var xhr = new XMLHttpRequest();
-	var body = 'num=' + encodeURIComponent(num) + '&col=' + encodeURIComponent(col) +
+	let action = "toCell";
+	let xhr = new XMLHttpRequest();
+	let body = 'num=' + encodeURIComponent(num) + '&col=' + encodeURIComponent(col) +
 	'&newValue=' + encodeURIComponent(newValue) + '&action=' + encodeURIComponent(action);
 	xhr.open("POST", myApp, true);
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
